19a5e2ccfcff7242d2d7e5a771474215f74c5193
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-06 11:37:23 +0900

    必要カラムの追加

---
+++ db/migrate/20200805125656_create_items.rb
@@ -0,0 +1,21 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      # t.text    :picture ,null:false
+      # t.string  :name ,null:false
+      # t.text    :text ,null:false
+      # t.integer :price ,null:false
+      # t.references :user_id ,null: false,foreign_key:true
+      # t.integer :category_id ,null:false
+      # t.integer :item_statue_id ,null:false
+      # t.integer :shipping_charge_id ,null:false
+      # t.integer :prefecture_id ,null:false
+      # t.integer :period_until_shipping_id ,null:false
+        # migrationはまだしていない
+
+      t.timestamps
+    end
+  end
+end
+
+
