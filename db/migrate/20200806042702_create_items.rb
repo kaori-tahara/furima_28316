47dc311666ce14d828c41a0e84aea63e65079e03
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-08 12:55:12 +0900

    userテーブルの作成など

--- db/migrate/20200806042702_create_items.rb
+++ db/migrate/20200806042702_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :name, null:false 
       t.text :text,null:false 
       t.integer :price,null:false 
-      t.references :user,null: false,foreign_key:true
+      t.references :user,foreign_key:true
       t.integer :category_id,null:false
       t.integer :item_statue_id,null:false
       t.integer :shipping_charge_id,null:false
