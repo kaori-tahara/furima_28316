6a60d500cdf70f3c2a47dc1ddf68850dee7b77f3
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-09 13:29:31 +0900

    gimei導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :user do
-    name {Faker::Name.last_name}
+    first_name {Faker::Japanese::Name.first_name}
+    family_name {Faker::Japanese::Name.last_name}
+    first_furigana {Gimei.first.katakana}
+    family_furigana {Gimei.last.katakana}
+    birth {Faker::Date.birthday}
+    nickname {Faker::Internet.username}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 8)
+
+    password = Faker::Internet.password(min_length: 6,mix_case: true)
     password {password}
     password_confirmation {password}
   end
