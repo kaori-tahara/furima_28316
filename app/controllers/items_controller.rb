861f01c1b9fe72edba93d1e938333e1e6143a4ed
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-11 11:09:53 +0900

    newの時に未ログイン者をログイン画面に遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,17 @@
 class ItemsController < ApplicationController
-
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user! , except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
-
+  
   def new
     @item = Item.new
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
+    item = Item.new(item_params)
+    if item.save
       redirect_to root_path
     else
       render :new
@@ -44,7 +43,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :text, :price, :picture, :category_id, :item_statue_id, :shipping_charge_id, :prefecture_id, :period_until_shipping_id).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
+  # def move_to_index
+  #   unless user_signed_in?
+  #    redirect_to action: :index 
+  #   end
+  # end
+
+
 end
