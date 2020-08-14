5f96bbf386049c8210b8cfb5d469c2ec16a761ab
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-14 11:57:33 +0900

    before_actionにdestroy追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
