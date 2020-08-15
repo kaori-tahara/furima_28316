668a196ae841a3eb4bdeacd7fd781d1109ea1669
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-15 18:19:38 +0900

    transactionモデル

---
+++ app/form/user_transaction.rb
@@ -0,0 +1,29 @@
+class UserTransaction
+
+  include ActiveModel::Model
+ 
+
+  attr_accessor :zip,:city,:address,:building_name,:phone_number,:prefecture
+
+
+
+  # shippingaddressのバリデーション
+  with_options presence: true do
+    validates :zip ,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address 
+    validates :phone_number,format: {with: /\A[0-9]{11}\z/, message: "ハイフン抜きの半角数字で入力してください"}
+  end
+
+  with_options presence: true, numericality: { greater_than: 1,message: "can't be blank" } do
+    validates :prefecture_id
+  end
+
+
+   def save
+    shippingaddress.create(zip: zip,city: city,address: address,building_name: building_name,phone_number: phone_number,prefecture_id: prefecture)
+  end
+
+
+
+end
