ee5c6c4bd2fca0761957c7469cec61dd9ba6d9b3
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-15 18:17:05 +0900

    migrationファイル作成

---
+++ db/migrate/20200814055146_create_shipping_addresses.rb
@@ -0,0 +1,17 @@
+class CreateShippingAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shipping_addresses do |t|
+
+      t.string :zip ,null: false 
+      t.string :city,null: false
+      t.string :address,null: false
+      t.string :building_name
+      t.string :phone_number,null: false
+      t.references :item ,foreign_key: true,null: false
+      t.integer :prefecture_id,null:false
+      t.references :user,null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
+
