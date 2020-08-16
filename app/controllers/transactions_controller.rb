dc610950fb57c7906a21dadef535512092c14864
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-16 23:31:39 +0900

    render消した

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,35 +5,34 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    @product_buyer = ShippingAddress.new(purchase_params)
-    if @product_buyer.valid?
+    @user_transaction = UserTransaction.new(purchase_params)
+    if @user_transaction.valid?
       pay_item
-      @product_buyer.save
-    redirect_to root_path, notice: 'ご購入を受け付けました。'
+      @user_transaction.save
+    #  @buyer = Purchase.find(params[:purchase_id])
+    redirect_to items_path
     else
-      render :index
+    redirect_to item_transactions_path
     end
   end
+
 
   private
 
   def purchase_params
-    params.permit(:token,:zip,:city,:address,:building_name,:phone_number,:prefecture_id,:item_id).merge(user_id: current_user.id)
+    params.permit(:zip,:city,:address,:building_name,:phone_number,:prefecture_id,:item_id).merge(user_id: current_user.id)
   end
+   
 
   def pay_item
     @item = Item.find(params[:item_id])
-    Payjp.api_key = sk_test_896aa3803241d953942ecf6a  # PAY.JPテスト秘密鍵
+    Payjp.api_key = 'sk_test_896aa3803241d953942ecf6a'# PAY.JPテスト秘密鍵
     Payjp::Charge.create(
       amount: @item.price,  # 商品の値段
-      card: purchase_params[:token],    # カードトークン
+      card: params[:token],    # カードトークン
       currency:'jpy'                 # 通貨の種類
-    )
+    ) 
   end
-
-  
-
-
 
 
 end
