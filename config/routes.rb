c22ea99e6879ef9495370b9a12a83900bc249847
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-17 20:13:45 +0900

    routing見直し

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-   resources :transactions
+   resources :transactions, only: [:index, :create]
   end
   resources :users, only: [:show]
+
+
+  
+
 end
